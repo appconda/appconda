3d6450187866cfe3957b201bde76ee34a40089db
selimtan <stanoncloud@gmail.com>
2024-10-15 14:56:49 +0300

    .

--- appconda/src/app/worker_test.ts
+++ appconda/src/app/worker_test.ts
@@ -1,14 +1,41 @@
 import { Appconda } from "../Appconda/Platform/Appconda";
 import { Console } from "../Tuval/CLI";
+import { Service } from "../Tuval/Platform/Service";
+import { register } from "./init";
 
 
-const platform = new Appconda();
-const worker = platform.getWorker();
-const workerName = 'mails';
+const start = async () => {
+    const pools = register.get('pools');
 
-worker.workerStart()
-    .action(() => {
-        Console.info(`Worker ${workerName} started`);
-    });
+    const workerName = 'mails';
+    
+    const platform = new Appconda();
+    
+    try {
+        platform.init(Service.TYPE_WORKER, {
+            workersNum: parseInt(process.env._APP_WORKERS_NUM || '1', 10),
+            connection: (await pools.get('queue').pop()).getResource(),
+            workerName: workerName.toLowerCase() ?? null,
+            queueName: workerName
+        });
+    } catch (e) {
+        Console.error(`${e.message}, File: ${e.file}, Line: ${e.line}`);
+    }
+    
+    const worker = platform.getWorker();
+    
+    
+    worker.workerStart()
+        .action(() => {
+            Console.info(`Worker ${workerName} started`);
+        });
 
-worker.start();
+        worker.shutdown()
+        .action(() => {
+            Console.info(`Worker ${workerName} shutdown`);
+        });
+    
+    worker.start();
+}
+
+start();
